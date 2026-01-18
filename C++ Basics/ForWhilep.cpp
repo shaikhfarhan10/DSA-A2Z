e20b18617f8d529f81a21c587cbbb01e015e9aaf
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-01-18 20:40:30 +0530

    Last Commit of the day

---
+++ C++ Basics/ForWhilep.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i = 1; i <= 10; i = i + 1)
+    {
+        cout << "Farhan" << endl;
+    }
+    while (i <= 10)
+    {
+        cout << "Farhan " << i << endl;
+        i = i + 1;
+    }
+}
--- C++ Basics/LearnCPP.cpp
+++ C++ Basics/LearnCPP.cpp
@@ -1,5 +1,40 @@
-#include <iostream>
-int main(){
-    std::cout<<"This is First Console of C++ in DSA A2Z";
-    return 0;
+#include <bits/stdc++.h>
+using namespace std;
+// int main()
+// {
+//     cout << "This is First Console of C++ in DSA A2Z" << endl
+//          << "This is Sencond Console" << endl;
+//     return 0;
+
+//     // This is to take value and Console it
+//     int x, y;
+//     cout << "Value of x:" << x << " and y:" << y;
+//     return 0;
+
+//     // This are some type of Datatype
+//     long p = 10000;
+//     long long q = 10000000000000000;
+//     int w;
+//     cin >> w;
+//     cout << "w:" << w << endl;
+//     int You = 10;
+//     string s1;
+//     cin >> s1;
+//     cout << s1;
+//     return 0;
+// }
+
+int main()
+{
+    // This is if else condition
+    int age;
+    cin >> age;
+    if (age >= 18)
+    {
+        cout << "you can drive" << endl;
+    }
+    if (age < 18)
+    {
+        cout << "you can't drive" << endl;
+    }
 }
