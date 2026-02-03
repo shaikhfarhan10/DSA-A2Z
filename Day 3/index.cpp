d9b7c4b7a6889c35e12d68e0509c3c6c9280fa9c
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-02-03 23:16:20 +0530

    Day 3 to A2Z DSA

---
+++ Day 3/index.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void display()
+{
+    cout << "This is the second Day of Doing this timepass without any coding" << endl;
+}
+
+int main()
+{
+    display();
+    return 0;
+}
