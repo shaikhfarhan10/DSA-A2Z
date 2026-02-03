8e7598656f0cc89259b56a1e7feb5e95bc5d945c
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-02-03 23:26:23 +0530

    Day 3 of DSA A2Z

--- Day 3/index.cpp
+++ Day 3/index.cpp
@@ -4,9 +4,36 @@ void display()
 {
     cout << "This is the second Day of Doing this timepass without any coding" << endl;
 }
+void curr()
+{
+    cout << "This is the second Day of Doing this timepass without any coding  and practicing" << endl;
+}
+void pump()
+{
+    cout << "This is the second Day of Doing this timepass without any coding  and practicing, just a prefect timepass" << endl;
+}
 
 int main()
 {
-    display();
+    int i = 0;
+    for (i = 0; i < 10; i++)
+    {
+        display();
+    }
+
+    int j = 0;
+    while (j < 10)
+    {
+        curr();
+        j++;
+    }
+
+    int k = 0;
+    do
+    {
+        pump();
+        k++;
+    } while (k < 10);
+
     return 0;
 }
