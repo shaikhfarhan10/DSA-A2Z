0527afdf85a4280ec6c127ff9e3c7c989af91429
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-02 23:23:06 +0530

    Day 31 to A2Z DSA

---
+++ 2nd - Learn Important Sorting Techniques/Sorting-1/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void BubbleSort(int arr[], int n)
+{
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the Size of your Array : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    BubbleSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
