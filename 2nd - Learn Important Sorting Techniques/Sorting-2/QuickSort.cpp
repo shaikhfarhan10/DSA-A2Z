a7e162bd125c06e6e7682fd502e97297f4602df3
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-08 23:03:10 +0530

    Day 36 to A2Z DSA

---
+++ 2nd - Learn Important Sorting Techniques/Sorting-2/QuickSort.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partiton(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            swap(arr[i], arr[j]);
+        }
+    }
+        swap(arr[low], arr[j]);
+        return j;
+}
+
+void quickS(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partiton(arr, low, high);
+        quickS(arr, low, pIndex - 1);
+        quickS(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> QuickSort(vector<int> &arr)
+{
+    quickS(arr, 0, arr.size() - 1);
+    return arr;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter the Size of your Array : ";
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    QuickSort(arr);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
