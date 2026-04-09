289e86df2051aec15abef6deb5bba2a5117a374d
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-09 22:04:56 +0530

    Day 37 to A2Z DSA

---
+++ 2nd - Learn Important Sorting Techniques/Revision Sorting-2/MergeSort.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void MS(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = ((low + high) / 2);
+    MS(arr, low, mid);
+    MS(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+void MergeSort(vector<int> &arr, int n)
+{
+    MS(arr, 0, n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the Size of Array : ";
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    MergeSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
