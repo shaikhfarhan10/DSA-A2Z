79f1f3f992342b6b1fc0c104f98016d08c849f58
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-10 23:16:53 +0530

    Day 38 to A2Z DSA

---
+++ 2nd - Learn Important Sorting Techniques/Revision Sorting-2/RecursiveInsertionSort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void recursiveInsertionSort(int arr[], int n)
+{
+    if (n <= 1)
+    {
+        return;
+    }
+    recursiveInsertionSort(arr, n - 1);
+
+    int j = n - 1;
+    while (j > 0 && arr[j - 1] > arr[j])
+    {
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+
+        j--;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the Size of your Array : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    recursiveInsertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
