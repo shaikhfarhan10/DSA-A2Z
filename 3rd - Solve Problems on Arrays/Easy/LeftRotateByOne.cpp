c8eec49bb8dd50f424ae2dee62d6da1ed4201dc5
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-21 22:49:50 +0530

    Day 49 to A2Z DSA

--- 3rd - Solve Problems on Arrays/Easy/LeftRotateByOne.cpp
+++ 3rd - Solve Problems on Arrays/Easy/LeftRotateByOne.cpp
@@ -1,2 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the Size of your Array : ";
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+}
