43910b9565b2e43262c9f8a0437c6ac405d384ae
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-22 12:00:00 +0530

    Day 50 to A2Z DSA

--- 3rd - Solve Problems on Arrays/Easy/LeftRotateByOne.cpp
+++ 3rd - Solve Problems on Arrays/Easy/LeftRotateByOne.cpp
@@ -1,14 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+// void LeftRotate(int arr[], int n)
+// {
+//     int temp = arr[0];
+//     for (int i = 0; i < n; i++)
+//     {
+//         arr[i - 1] = arr[i];
+//     }
+//     arr[n - 1] = temp;
+// }
+
+int main()
+{
     int n;
     cout << "Enter the Size of your Array : ";
-    cin>>n;
+    cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    
+    cout << "Your Array : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    LeftRotate(arr, n);
+    cout << endl;
+    cout << "Left Side Rotated : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
