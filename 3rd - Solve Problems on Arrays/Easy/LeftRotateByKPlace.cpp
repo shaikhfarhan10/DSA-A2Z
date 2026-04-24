4010ca1f5b901d2ed1aa3a1129fcc66787b6ff7f
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-04-24 21:50:35 +0530

    Day 52 to A2Z DSA

--- 3rd - Solve Problems on Arrays/Easy/LeftRotateByKPlace.cpp
+++ 3rd - Solve Problems on Arrays/Easy/LeftRotateByKPlace.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 void LeftRotateByKPlace(int arr[], int n, int k)
 {
+    k = k % n;
+    int temp[k];
+    for (int i = 0; i < k; i++)
+    {
+        temp[i] = arr[i];
+    }
+    for (int i = k; i < n; i++)
+    {
+        arr[i - k] = arr[i];
+    }
+
+    for (int i = n - k; i < n; i++)
+    {
+        arr[i] = temp[i - (n - k)];
+    }
 }
 
 int main()
