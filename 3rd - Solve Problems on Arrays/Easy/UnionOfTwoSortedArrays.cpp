60aaba2bf4c6003ffd7b133ddcc9a23a38f0581e
shaikhfarhan10 <shaikhfarhansalim10@gmail.com>
2026-05-04 23:27:56 +0530

    Day 62 to A2Z DSA

--- 3rd - Solve Problems on Arrays/Easy/UnionOfTwoSortedArrays.cpp
+++ 3rd - Solve Problems on Arrays/Easy/UnionOfTwoSortedArrays.cpp
@@ -5,5 +5,10 @@ int main()
 {
     int n;
     cout << "Enter the Size of your Array : ";
-    cin>>n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
 }
